a339314d8bfc2ffb6e2a38dc68203925f9453355
ajszoke <zoke2556@gmail.com>
2018-03-30 19:17:16 -0400

    Documentation and cleanup

--- InstructionMemory.java
+++ InstructionMemory.java
@@ -1,3 +1,9 @@
+/*
+ * This class is responsible for iterating the processor through the program. It reads in text lines from the test file one at a
+ * time, then computes and returns their byte array equivalents to simulate a processor's structure. As the test files have (supposedly)
+ * already been examined to ensure accuracy and validity, it provides no handling for malformed instructions.
+ */
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -6,7 +12,6 @@ public class InstructionMemory {
 	
 	private Scanner input;
 	private String line;
-//	private BitSet bs;
 	
 	public InstructionMemory(File f) throws FileNotFoundException {
 		input = new Scanner(f);
@@ -19,6 +24,7 @@ public class InstructionMemory {
 			String tempStr;
 			int temp = 0;
 			
+			// first compute the unsigned int equivalent of the 32-bit instruction, then cast it to a byte array
 			for (int i = 0; i < 4; i++) {
 				tempStr = line.substring(i*8, (i+1)*8);
 				temp = 0;
@@ -28,12 +34,6 @@ public class InstructionMemory {
 				res[i] = (byte) temp;
 			}
 			return res;
-			
-//			bs = new BitSet(Processor.WORDSIZE);
-//			for (int i = 0; i < Processor.WORDSIZE; i++) {
-//				if (line.charAt(i) == '1') bs.set(i);
-//			}
-//			return bs.toByteArray();
 		}
 		else return Processor.NAN;
 	}
